c653ba0ad73ad797c54a2fe6c31403797b9203cd
Lova Ramih <lova.ramiharisoa@gmail.com>
2024-05-04 07:14:26 +0300

    création des services axios

---
+++ src/_services/account_service.js
@@ -0,0 +1,6 @@
+//** import des modules nécessaires */
+import Axios from "./caller_service";
+
+let login = (credentials) => {
+    return
+}
---
+++ src/_services/caller_service.js
@@ -0,0 +1,8 @@
+/** import des models nécessaires */
+import axios from 'axios'
+
+const Axios = axios.create(
+    baseUrl = 'http://localhost/8000'
+);
+
+export default Axios
